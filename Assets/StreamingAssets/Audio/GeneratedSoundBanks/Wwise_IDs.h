f61d99cdbd41b5787c457f604fb97de3ca9c8154
RobbeDenis <robbe.denis@gmail.com>
2023-06-04 23:27:05 +0200

    Added ambient

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_SX_AMBIENCE = 2921826070U;
         static const AkUniqueID PLAY_SX_BARREL_HIT = 2076655380U;
         static const AkUniqueID PLAY_SX_BOSS_ATTACK = 2366761924U;
         static const AkUniqueID PLAY_SX_BOSS_DEATH = 875007526U;
